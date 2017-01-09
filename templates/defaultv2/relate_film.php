2ebe32d68318d32dedb3708ff8a7289ed3db6e6a
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2017-01-09 16:15:17 +0700

    style

--- templates/defaultv2/relate_film.php
+++ templates/defaultv2/relate_film.php
@@ -1,3 +1,8 @@
-<div class="inner">
-                                            <a class="poster" href="<?=$filmURL;?>" title="<?=$filmNAMEVN;?> - <?=$filmNAMEEN;?>"> <img src="<?=$filmIMG;?>" alt="<?=$filmNAMEVN;?>"> </a> <span class="status"><?=$Status;?></span> <a class="name" href="<?=$filmURL;?>" title="<?=$filmNAMEVN;?> - <?=$filmNAMEEN;?>"><?=$filmNAMEVN;?></a> <dfn><?=$filmNAMEEN;?></dfn> <dfn><?=$filmYEAR;?></dfn> </div>
+<div class="item col-lg-3 col-md-3 col-sm-6 col-xs-6">
+
+    <a class="poster" href="<?=$filmURL;?>" title="<?=$filmNAMEVN;?> - <?=$filmNAMEEN;?>">
+        <img src="<?=$filmIMG;?>" alt="<?=$filmNAMEVN;?>"> </a> <span class="status"><?=$Status;?></span>
+    <a class="name" href="<?=$filmURL;?>" title="<?=$filmNAMEVN;?> - <?=$filmNAMEEN;?>"><?=$filmNAMEVN;?></a>
+    <dfn><?=$filmNAMEEN;?></dfn> <dfn><?=$filmYEAR;?></dfn>
+</div>
                                    
