ac81bc6ad331ea8990137a8192ec4efcacca7697
PHAM XUAN THUY <thuypx1983@gmail.com>
2017-01-09 23:35:45 +0700

    footer update

--- statics/defaultv2/js/pl.watchv2.js
+++ statics/defaultv2/js/pl.watchv2.js
@@ -544,8 +544,13 @@ jQuery(document)['ready'](function (t) {
             var offset = jQuery('.block.servers').offset();
             var sidebarTopMargin =  offset.top - jQuery('.block.servers').outerHeight( true ) - 273;
 
+            var link = $('.block.media');
+            var position = link.position(); //cache the position
+            var bottom = $(window).height() - position.top - link.height();
+
             jQuery('.sidebar.col-lg-4.col-md-4')['animate']({
-                marginTop: sidebarTopMargin
+                //marginTop: sidebarTopMargin
+                marginTop: bottom
             });
             jQuery('.block.media')['animate']({
                 "width": newWidth
