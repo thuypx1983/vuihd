66eeecccba2e520780898a926fbcfe80a8881f19
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2017-01-09 16:12:46 +0700

    style

--- templates/defaultv2/home-info.php
+++ templates/defaultv2/home-info.php
@@ -252,12 +252,12 @@ if(($filmSub == 0) && ($filmLB == 0)){$subscribe = 0;}elseif($filmLB == 2){$subs
                         <!--.block-->
                         <div class="block list-film-slide">
                             <div class="widget-title">
-     							<h3 class="title">Phim liên quan sss</h3>
+     							<h3 class="title">Phim liên quan</h3>
 								</div> 
                             <div class="block-body slider">
                                 <div class="control prev"></div>
                                 <div class="control next"></div>
-                                <div class="list-film row" id="pl-slidez">
+                                <div class="list-film row" id="pl-slidez1">
                                     
                                      <?=ShowFilm("WHERE film_id <> '".$filmID."' AND (MATCH (film_name,film_name_real,film_name_ascii,film_tag,film_tag_ascii) AGAINST ('".text_preg_replace($filmNAMEVN." ".$filmNAMEEN)."' IN BOOLEAN MODE) OR film_cat LIKE '%".$row['film_cat']."%')","ORDER BY MATCH (film_name,film_name_real,film_name_ascii,film_tag,film_tag_ascii) AGAINST ('".text_preg_replace($filmNAMEVN." ".$filmNAMEEN)."' IN BOOLEAN MODE) ",8,"relate_film","");?>
                                 </div>
