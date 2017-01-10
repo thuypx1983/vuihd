e38b9f7b1aca46e6f900d5be66e71559ff5d4baa
PHAM XUAN THUY <thuypx1983@gmail.com>
2017-01-10 23:14:53 +0700

    add font

--- templates/defaultv2/styles.php
+++ templates/defaultv2/styles.php
@@ -14,14 +14,26 @@
     var	MAIN_URL	=	'<?=$web_link;?>';
     var	AjaxURL	=	'<?=$web_link;?>/ajax';
 </script>
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.5";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+
+ <script>
+  window.fbAsyncInit = function() {
+   FB.init({
+    appId      : '220529061684267',
+    xfbml      : true,
+    version    : 'v2.8'
+   });
+  };
+
+  (function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+ </script>
+
+
 <script src="https://apis.google.com/js/platform.js" async defer></script>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
