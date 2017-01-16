6f492b37eacfb4795f5fae679df7f97fb6e41af8
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2017-01-16 11:48:57 +0700

    test pop-up

--- templates/defaultv2/footer.php
+++ templates/defaultv2/footer.php
@@ -64,7 +64,10 @@ _atrk_opts = { atrk_acct:"LM/Rm1aMp4Z36C", domain:"vuihd.com",dynamic: true};
 <!-- Like so: -->
 <a href="#test-popup" class="open-popup-link">Show inline popup</a>
 <script type="text/javascript">
-
+    $('.open-popup-link').magnificPopup({
+        type:'inline',
+        midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    });
 </script>
 
 
