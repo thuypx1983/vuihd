f0c523591a724ca74b65e505fa809fc608ea6f05
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2017-01-23 09:34:37 +0700

    news relate

--- templates/defaultv2/shownews_related.php
+++ templates/defaultv2/shownews_related.php
@@ -1,6 +1,11 @@
 <?
 $html .= '<div class="item">
-                                            <div class="thumb"> <img src="'.$row['news_img'].'" alt="'.$row['news_name'].'"> </div>
-                                            <div class="info"> <a class="name" href="/tin-tuc/'.$row['news_url'].'/'.$row['news_id'].'.html" title="'.$row['news_name'].'">'.$row['news_name'].'</a> <dfn class="status"><i class="fa fa-quote-left"></i> '.$rows['news_cat'].'</dfn> </div>
-                                        </div>';
+<div class="thumb"> <img src="'.$row['news_img'].'" alt="'.$row['news_name'].'"> </div>
+
+<div class="info">
+<a class="name" href="/tin-tuc/'.$row['news_url'].'/'.$row['news_id'].'.html" title="'.$row['news_name'].'">'.$row['news_name'].'</a>
+ <dfn class="status"><i class="fa fa-quote-left"></i> '.$rows['news_cat'].'</dfn>
+ </div>
+
+</div>';
 ?>
