ae8ee8226cf54f6ec9bb0fcb4a3c955810024f1e
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2017-01-23 09:55:09 +0700

    news relate

--- templates/defaultv2/shownews_related.php
+++ templates/defaultv2/shownews_related.php
@@ -1,7 +1,7 @@
 <?
 $html .= '<div class="news-item">
 <div class="col-md-5 col-lg-5">
-<div class="thumb"> <img src="'.$row['news_img'].'" alt="'.$row['news_name'].'"> </div>
+<div class="thumb"> <img style="max-width: 100%" src="'.$row['news_img'].'" alt="'.$row['news_name'].'"> </div>
 </div>
 <div class="col-md-7 col-lg-7">
     <div class="info">
