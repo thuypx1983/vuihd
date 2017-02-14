4fb34e32d1d4e0fe74f34b9271c63ad30e3de931
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2017-02-14 14:01:21 +0700

    vietlott

--- includes/vietlott.php
+++ includes/vietlott.php
@@ -24,4 +24,31 @@ class vietlott{
         }
         return $result;
     }
+    function filterUserByNumber() {
+        $agr=func_num_args();
+
+        switch($agr){
+            case 1:
+                $sql="SELECT * FROM `vietlott` AS table1 WHERE table1.number1 =".func_get_arg(0)." OR table1.number2 =".func_get_arg(0)." OR table1.number3 =".func_get_arg(0);
+                break;
+                break;
+            case 2:
+              $sql="SELECT * FROM
+              (SELECT * FROM `vietlott` AS table1 WHERE table1.number1 =".func_get_arg(0)." OR table1.number2 =".func_get_arg(0)." OR table1.number3 =".func_get_arg(0).") AS table1
+                  WHERE table1.number1=".func_get_arg(1)." or table1.number2=".func_get_arg(1)." or table1.number3=".func_get_arg(1);
+                break;
+            case 3:
+                $sql="SELECT * from
+             (SELECT * FROM
+              (SELECT * FROM `vietlott` AS table1 WHERE table1.number1 =".func_get_arg(0)." OR table1.number2 =".func_get_arg(0)." OR table1.number3 =".func_get_arg(0).") AS table1
+                  WHERE table1.number1=".func_get_arg(1)." or table1.number2=".func_get_arg(1)." or table1.number3=".func_get_arg(1).")  AS table3
+                    WHERE table3.number1=".func_get_arg(2)." OR table3.number2=".func_get_arg(2)." OR table3.number3=".func_get_arg(2)."";
+                break;
+            default:
+                $sql="SELECT * from `vietlott`";
+                break;
+
+        }
+        return $sql;
+    }
 }
