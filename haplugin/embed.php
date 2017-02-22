96fbb708ec9d8b67070b8b3e7667cc8859fdd388
vuihd.com <vuihd_com@packer-debian-8-amd64.droplet.local>
2017-02-22 08:26:15 +0000

    hap plugin

--- haplugin/embed.php
+++ haplugin/embed.php
@@ -16,7 +16,9 @@ if(isset($_POST['a'])&&isset($_POST['n'])&&isset($_POST['h'])&&isset($_POST['y']
 	$url = urlencode($url[1]);
 	$sub = $haplugin->decode(base64_decode(base64_decode($_POST['n'])),ENCODEK);
 	$share = base64_decode($_POST['h']);
-	$share = base64_decode($_POST['y']);
+	$poster = base64_decode($_POST['y']);
+	$cat = base64_decode($_POST['e']);
+	$cou = base64_decode($_POST['u']);
 	if($sub!=NULL)
 	{
 		$subtitle = NULL;
@@ -71,62 +73,28 @@ if(isset($_POST['a'])&&isset($_POST['n'])&&isset($_POST['h'])&&isset($_POST['y']
 									$dataplay .= '{label: "'.$quality.'", file: "'.$haurl.'", type: "mp4"},';
 								}
 							}
-						}
-						$playerdata = array
-						(
-							'source' => ''.$dataplay.'',
-							'skin' => '19',
-							'jwkey' => 'Fgvbf/xGYQG05kB2SU4YfEcBSik2EkZCxK2ErTgvfSE=',
-							'options' => array
-							(
-								'autostart' => 'true',
-								'mute' => 'false',
-								'preload' => 'false',
-								'repeat' => 'false',
-								'stagevideo' => 'false',
-								'displaydescription' => 'false',
-								'displaytitle' => 'false',
-								'width' => '100%',
-								'height' => '100%',
-								'stretching' => 'uniform',
-								'aspectratio' => '16:9',
-								'primary' => 'html5',
-								'image' => ''.$poster.'',
-							),
-							'logo' => array
-							(
-								'file' => 'http://vuihd.com/newplayer/logo.png',
-								'position' => 'top-left',
-								'link' => 'http://vuihd.com',
-								'abouttext' => 'VuiHD.COM Premium Player',
-								'aboutlink' => 'http://vuihd.com',
-							),
-							/*'resize' => array
-							(
-								'szoom' => '640',
-								'wzoom' => '640',
-								'hzoom' => '480',
-								'wdefault' => '100%',
-								'hdefault' => '100%',
-							),*/
-							'subtitle' => ''.$subtitle.'',
-							'share' => ''.$share.'',
-							'download' => false,
-							'speed' => false,
-							'videoads' => false,
-							'seek' => true,
-							'googleads' => '',
-							/*'bannerads' => array
-							(
-								'link' => 'http://haplugins.com',
-								'embed' => '',
-							),*/
-						);
-						 $haplayer = new JWPlayer7;
+						 }
+						 $playerdata = array(
+							//Source and Thumb
+							'source' => '{"sources":['.$dataplay.'],"image":"'.$poster.'",captions: captions,tracks :['.$subtitle.']}',
+							//Width and Height
+							'width' => '100%',
+							'height' => '100%',
+							//Logo
+							'logo_file' => 'http://vuihd.com/newplayer/logo.png',
+							'logo_link' => 'http://vuihd.com',
+							'logo_position' => 'top-left', //top-left, top-right, bottom-left, bottom-right
+							//Ads
+							'videoads' => true,
+							'cat' => $cat,
+							'country' => $cou,
+							'mobile' => $haplugin->mobile(),
+						 );
+						 $haplayer = new JWPlayer;
 						 $source = json_encode($playerdata);
 						 $haplayer->ssource($source);
-						// $haload .= '<head><title>HAPLUGINS - VIDEO EMBED</title><meta name="ROBOTS" content="NOINDEX,NOFOLLOW"><style type="text/css">body{-webkit-touch-callout:none;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;-o-user-select:none;user-select:none;background-color:#000}.pop-up{display:none;position:fixed;z-index:1;padding-top:100px;left:0;top:0;width:100%;height:100%;overflow:auto;background-color:#000;background-color:rgba(0,0,0,0.4)}.pop-up-content{position:relative;background-color:#fefefe;margin:auto;padding:0;border:1px solid #888;width:40%;box-shadow:0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);-webkit-animation-name:animatetop;-webkit-animation-duration:.4s;animation-name:animatetop;animation-duration:.4s}@-webkit-keyframes animatetop{from{top:-300px;opacity:0}to{top:0;opacity:1}}@keyframes animatetop{from{top:-300px;opacity:0}to{top:0;opacity:1}}.pop-up-header{padding:2px 16px;background-color:#5cb85c;color:#fff}.pop-up-body{padding:2px 16px}</style><script type="text/javascript">/*<![CDATA[*/var message="NoRightClicking";function defeatIE(){if(document.all){(message);return false}}function defeatNS(a){if(document.layers||(document.getElementById&&!document.all)){if(a.which==2||a.which==3){(message);return false}}}if(document.layers){document.captureEvents(Event.MOUSEDOWN);document.onmousedown=defeatNS}else{document.onmouseup=defeatNS;document.oncontextmenu=defeatIE}document.oncontextmenu=new Function("return false");document.onkeydown=function(a){if(a.ctrlKey&&(a.keyCode===67||a.keyCode===86||a.keyCode===85||a.keyCode===117)){}return false};window.top.location!=window.location&&(window.location.href="/");function reload(){};/*]]>function popup(a){var b=document.getElementById(a);b.style.display="block",window.onclick=function(a){a.target==b&&(b.style.display="none")},document.onkeydown=function(a){a=a||window.event;var c=!1;c="key"in a?"Escape"==a.key||"Esc"==a.key:27==a.keyCode,c&&(b.style.display="none")}}*/</script>';
-							$haload .= $haplayer->result();
+						 //$haload .= '<!DOCTYPE html><html><head><title>HAPLUGINS - VIDEO EMBED</title><meta name="ROBOTS" content="NOINDEX,NOFOLLOW"><style type="text/css">body{-webkit-touch-callout:none;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;-o-user-select:none;user-select:none;background-color:#000}.pop-up{display:none;position:fixed;z-index:1;padding-top:100px;left:0;top:0;width:100%;height:100%;overflow:auto;background-color:#000;background-color:rgba(0,0,0,0.4)}.pop-up-content{position:relative;background-color:#fefefe;margin:auto;padding:0;border:1px solid #888;width:40%;box-shadow:0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);-webkit-animation-name:animatetop;-webkit-animation-duration:.4s;animation-name:animatetop;animation-duration:.4s}@-webkit-keyframes animatetop{from{top:-300px;opacity:0}to{top:0;opacity:1}}@keyframes animatetop{from{top:-300px;opacity:0}to{top:0;opacity:1}}.pop-up-header{padding:2px 16px;background-color:#5cb85c;color:#fff}.pop-up-body{padding:2px 16px}</style><script type="text/javascript">/*<![CDATA[*/var message="NoRightClicking";function defeatIE(){if(document.all){(message);return false}}function defeatNS(a){if(document.layers||(document.getElementById&&!document.all)){if(a.which==2||a.which==3){(message);return false}}}if(document.layers){document.captureEvents(Event.MOUSEDOWN);document.onmousedown=defeatNS}else{document.onmouseup=defeatNS;document.oncontextmenu=defeatIE}document.oncontextmenu=new Function("return false");document.onkeydown=function(a){if(a.ctrlKey&&(a.keyCode===67||a.keyCode===86||a.keyCode===85||a.keyCode===117)){}return false};window.top.location!=window.location&&(window.location.href="/");function reload(){};/*]]>function popup(a){var b=document.getElementById(a);b.style.display="block",window.onclick=function(a){a.target==b&&(b.style.display="none")},document.onkeydown=function(a){a=a||window.event;var c=!1;c="key"in a?"Escape"==a.key||"Esc"==a.key:27==a.keyCode,c&&(b.style.display="none")}}*/</script></head><body>';
+						$haload .= $haplayer->result();
 						$cache->stext($haload);
 						$cache->supercache();
 					}
--- haplugin/jwplayer.class.php
+++ haplugin/jwplayer.class.php
@@ -2,7 +2,7 @@
 /**
  * PHP code library developed by Hoang Anh
  *
- * @package     JWPlayer 7 - Ver 1.0
+ * @package     JWPlayer 7 - Ver 3.0
  * @author      Hoang Anh
  * @copyright   Copyright (c) 2017, Hoang Anh
  * @license     http://www.opensource.org/licenses/bsd-license.php
@@ -10,217 +10,90 @@
  **/
  define('NAME', $_SERVER['SERVER_NAME']);
  define('DIR', dirname($_SERVER['PHP_SELF']));
- class JWPlayer7
+ class JWPlayer
  {
 	 private $__source;
 	 private $__player;
 	 private $__temp;
-	 public function ssource($a){$this->__source = $a;}
+	 public function ssource($temp) {$this->__source=$temp!=NULL?$temp:NULL;}
+ 	 public function splayer($temp) {$this->__player=$temp!=NULL?$temp:NULL;}
+	 public function stemp($temp) {$this->__temp=$temp!=NULL?$temp:NULL;}
+	 public function mtemp($temp){$this->__temp .= $temp;}
 	 public function rsource(){$this->__source = NULL;}
-	 public function gsource(){return $this->__source;}
-	 public function stemp($a){$this->__temp = $a;}
-	 public function rtemp(){$this->__temp = NULL;}
-	 public function gtemp(){return $this->__temp;}
-	 public function mtemp($a){$this->__temp .= $a;}
-	 public function splayer($a){$this->__player = $a;}
+ 	 public function rtemp(){$this->__temp = NULL;}
 	 public function rplayer(){$this->__player = NULL;}
+	 public function gsource(){return $this->__source;}
+	 public function gtemp(){return $this->__temp;}
  	 public function gplayer(){return $this->__player;}
-	 private function __handle()
+	 private function read($temp)
 	 {
-		 $ha = array();
-		 $ha = json_decode($this->gsource());
-		 $this->rtemp();
-		 $this->mtemp($this->__html(1));
-		 if(isset($ha->{'bannerads'})&&$ha->{'bannerads'}!=NULL)
-		 {
-			$this->mtemp($this->__html(4,$ha->{'bannerads'})); 
-		 }
-		 if(isset($ha->{'jwkey'})&&$ha->{'jwkey'}!=NULL)
-		 {
-			$this->mtemp($this->__html(3,$ha->{'jwkey'})); 
-		 }
-		 if(isset($ha->{'skin'})&&$ha->{'skin'}!=NULL)
-		 {
-			$this->mtemp($this->__html(5,$this->__skin($ha->{'skin'},true)));
-		 }
-		 $this->mtemp('player.setup({');
-		 if(isset($ha->{'source'})&&$ha->{'source'}!=NULL)
-		 {
-			$this->mtemp($this->__html(2,$ha->{'source'})); 
-		 }
-		 if(isset($ha->{'options'})&&$ha->{'options'}!=NULL)
-		 {
-			$this->mtemp($this->__html(5,$this->__options($ha->{'options'}))); 
-		 }
-		 if(isset($ha->{'skin'})&&$ha->{'skin'}!=NULL)
-		 {
-			$this->mtemp($this->__html(5,$this->__skin($ha->{'skin'},false))); 
-		 }
-		 if(isset($ha->{'share'})&&$ha->{'share'}!=NULL)
-		 {
-			$this->mtemp($this->__html(9,$ha->{'share'})); 
-		 }
-		 if(isset($ha->{'videoads'})&&$ha->{'videoads'}!=NULL)
-		 {
-			$this->mtemp($this->__html(13,$ha->{'videoads'})); 
-		 }
-		 if(isset($ha->{'googleads'})&&$ha->{'googleads'}!=NULL)
-		 {
-			$this->mtemp($this->__html(14,$ha->{'googleads'})); 
-		 }
-		 if(isset($ha->{'logo'})&&$ha->{'logo'}!=NULL)
-		 {
-			 $this->mtemp($this->__html(5,$this->__logo($ha->{'logo'}))); 
-		 }
-		 if(isset($ha->{'subtitle'})&&$ha->{'subtitle'}!=NULL)
-		 {
-			$this->mtemp($this->__html(5,$this->__subtitle($ha->{'subtitle'}))); 
-		 }
-		 $this->mtemp('});</script>');
-		 $this->mtemp('<script type="text/javascript">');
-		 if(isset($ha->{'resize'})&&$ha->{'resize'}!=NULL)
-		 {
-			$this->mtemp($this->__html(5,$this->__resize($ha->{'resize'}))); 
-		 }
-		 if(isset($ha->{'download'})&&$ha->{'download'}!=NULL)
-		 {
-			$this->mtemp($this->__html(10)); 
-		 }
-		 if(isset($ha->{'speed'})&&$ha->{'speed'}!=NULL)
-		 {
-			$this->mtemp($this->__html(11)); 
-		 }
-		 if(isset($ha->{'seek'})&&$ha->{'seek'}!=NULL)
-		 {
-			$this->mtemp($this->__html(5,$this->__seek($ha->{'seek'}))); 
-		 }
-		 $this->mtemp('</script>');
-		 $this->rplayer();
-		 $this->splayer($this->gtemp());
-		 return $this->gplayer();
+		$ha = array();
+		$ha = json_decode($this->gsource());
+		if(isset($ha->{$temp})&&$ha->{$temp}!=NULL) return $ha->{$temp};
+		else return false;
 	 }
-	 private function __html($code,$string=NULL)
+	 private function __handle($temp)
 	 {
-		 switch($code)
+	 	file_put_contents('/home/vuihd_com/public_html/__handle.txt',$temp);
+		 if($temp == 'share' && $this->read($temp)) return 'sharing:{link: "'.$this->read('share').'",heading: "Share"},';
+		 elseif($temp == 'download' && $this->read($temp)) return 'download();';
+		 elseif($temp == 'videoads' && $this->read($temp)) 
 		 {
-			 case 1: return '<link rel="stylesheet" href="//'.NAME.''.DIR.'/asset/css/player.css"><script type="text/javascript" src="//'.NAME.''.DIR.'/asset/js/jquery.js"></script><script type="text/javascript" src="//'.NAME.''.DIR.'/asset/js/alert.js"></script><script type="text/javascript" src="//'.NAME.''.DIR.'/asset/js/player.js"></script>'; break;
- 			 case 2: return 'sources:['.$string.'],flasplayer: "//'.NAME.''.DIR.'/asset/skin/jwplayer.swf",'; break;
-			 case 3: return '<script type="text/javascript">jwplayer.key="'.$string.'";</script><div id="player"></div><script type="text/javascript">var player = jwplayer("player");'; break;
-			 case 4: return '<div class="hideads"><div id="hideads"><div class="contentads">'.$string->{'embed'}.'</div><a href="'.$string->{'link'}.'" target="_blank" id="in_player_close" class="in_player_close">Close and Play</a></div></div>'; break;
-			 case 5: return $string; break;
-			 case 9: return 'sharing:{link: "'.$string.'",heading: "Share"},'; break;
-			 case 10: return 'download();'; break;
-			 case 11: return 'speed();'; break;
-			 case 13: return 'advertising: {client: "vast",companiondiv: {id: "adrectangle", height: 250, width: 300},schedule: "//'.NAME.''.DIR.'/asset/xml/vmap.xml"},'; break;
-			 case 14: return 'advertising: {client: "googima",offset: "pre",tag: "'.$string.'"},'; break;
-			 default: return NULL;
+			 if($this->read('mobile'))
+			 {
+				 //Ads on mobile
+				 return 'ads :{video: [{position : 0,link : ["http://blueserving.com/vast.xml?key=0cf81923f34c497f07efc0a31c3d0e4d&cat='.$this->read('cat').'&country='.$this->read('country').'",]}],overlay: [{type : "tags",	position : 10, time : 30,link : ["//'.NAME.''.DIR.'/assets/xml/overlay.xml"]}],},';
+			 }
+			 else
+			 {
+				 //Ads on PC
+				 return 'ads :{video: [{position : 0,link : ["http://blueserving.com/vast.xml?key=a52c8f53872b2b99bd78d6e44a4fd141&cat='.$this->read('cat').'&country='.$this->read('country').'",]}],overlay: [{type : "tags",	position : 10, time : 30,link : ["//'.NAME.''.DIR.'/assets/xml/overlay.xml"]}],},';
+			 }
 		 }
+		 
 	 }
-	 private function __resize($string)
+	 private function __html()
 	 {
-		 $resize = new JWPlayer7;
-		 $resize->rtemp();
-		 $resize->mtemp('zoom("'.$string->{'szoom'}.'","'.$string->{'wzoom'}.'","'.$string->{'hzoom'}.'","'.$string->{'wdefault'}.'","'.$string->{'hdefault'}.'");');
-		 return $resize->gtemp();
-	 }
-	 private function __subtitle($string)
-	 {
-		 $sub = new JWPlayer7;
-		 $sub->rtemp();
-		 $sub->mtemp('"tracks": [');
-		 for($i=0;$i<count($string);$i++)
-		 {
-			$sub->mtemp($string);
-		 }
-		 $sub->mtemp('],captions: {	color: "#99FFFF",	fontSize: 24,	backgroundOpacity: 5,	edgeStyle: "uniform",	backgroundColor: "#FFFFFF",	},');
-		 return $sub->gtemp();
-	 }
-	 private function __options($string)
-	 {
-		 $opt = new JWPlayer7;
-		 $opt->rtemp();
-		 foreach($string as $key => $value)
-		 {
-			 if($value!='DISALLOW'){$opt->mtemp('"'.$key.'":"'.$value.'",');}
-			 else{$opt->mtemp(NULL);}
-		 }
-		 return $opt->gtemp();
-	 }
-	 private function __seek($string)
-	 {
-		 $seek = new JWPlayer7;
-		 $seek->rtemp();
-		 if($string==true)
-		 {
-				$seek->mtemp('player.on("ready", function() {
-					var y = encodeURI(window.location.href);
-					var x = getcookie(""+y+"");
-					if(x!=null&&x!=0)
-					{
-						swal({   title: "Bạn Có Muốn Xem Tiếp",   text: "VuiHD nhận thấy lần trước bạn đang dừng ở giây "+x+", bạn có muốn xem tiếp hay không?",   type: "warning",   showCancelButton: true,   confirmButtonColor: "#DD6B55",   confirmButtonText: "Xem Tiếp",   closeOnConfirm: true }, function(){
-							jwplayer().seek(x);
-						});
-					}					
-				});');
-		 }
-		 else {$seek->mtemp(NULL);}
-		 return $seek->gtemp();
-	 }
-	 private function __logo($string)
-	 {
-		 $logo = new JWPlayer7;
-		 $logo->rtemp();
-		 $logo->mtemp('logo: {file: "'.$string->{'file'}.'",link: "'.$string->{'link'}.'",position: "'.$string->{'position'}.'"},abouttext: "'.$string->{'abouttext'}.'", aboutlink: "'.$string->{'aboutlink'}.'",');
-		 return $logo->gtemp();
-	 }
-	 private function __skin($string,$case)
-	 {
-		 $ski = new JWPlayer7;
-		 $ski->rtemp();
-		 switch(intval($string))
-		 {
-			case 1: $skin = ""; $color = 'red'; break;
-			case 2: $skin = "thin"; $color = 'red'; break;
-			case 3: $skin = "thin"; $color = 'blue'; break;
-			case 4: $skin = "thin"; $color = 'green'; break;
-			case 5: $skin = "thin"; $color = 'purple'; break;
-			case 6: $skin = "thin"; $color = 'violet'; break;
-			case 7: $skin = "thin"; $color = 'yellow'; break;
-			case 8: $skin = "bekle"; $color = 'red'; break;
-			case 9: $skin = "beelden"; $color = 'red'; break;
-			case 10: $skin = "five"; $color = 'red'; break;
-			case 11: $skin = "glow"; $color = 'red'; break;
-			case 12: $skin = "roundster"; $color = 'red'; break;
-			case 13: $skin = "seven"; $color = 'red'; break;
-			case 14: $skin = "six"; $color = 'red'; break;
-			case 15: $skin = "stormtrooper"; $color = 'red'; break;
-			case 16: $skin = "vapor"; $color = 'red'; break;
-			case 17: $skin = "seven"; $color = 'vip1'; break;
-			case 18: $skin = "flat"; $color = 'vip2'; break;
-			case 19: $skin = "haplugin"; $color = 'haplugin'; break;
-			default: $skin = NULL; $color = 'red'; break;
-		 }
-		 if($case==true)
-		 {
-			$ski->mtemp('var filename = "//'.NAME.''.DIR.'/asset/skin/skin-'.$color.'.css";
-			var _color = location.search.split("color=")[1];
-			if (_color != undefined && _color != "" && _color != "red")
-			filename = "//'.NAME.''.DIR.'/asset/skin/skin-" + _color + ".css";
-			var fileref = document.createElement("link");
-			fileref.setAttribute("rel", "stylesheet");
-			fileref.setAttribute("type", "text/css");
-			fileref.setAttribute("href", filename);
-			document.getElementsByTagName("head")[0].appendChild(fileref);');
-		 }
-		 else
-		 {
-			 $ski->mtemp('skin: {name:"'.$skin.'"},active:"#ccc",inactive:"#c91c54",background:"#141414",');
-		 }
-		 return $ski->gtemp();
+		$html = NULL;
+		$html = '<script type="text/javascript" src="//'.NAME.''.DIR.'/assets/js/player.js"></script>
+				<div id="player-content"><div class="cssload-loading">Loading Player ...</div></div>
+				<script type="text/javascript">
+				var captions = {
+					color: \'#FFFFFF\',
+					fontSize: 20,
+					fontFamily: \'"Comic Sans MS", cursive, sans-serif\',
+					fontOpacity: 100,
+					backgroundOpacity: 75,
+					edgeStyle: \'dropshadow\',
+					windowColor: \'#000000\',
+					windowOpacity: 25
+				};
+				jwplayer("player-content").setup({
+					flashplayer : "//'.NAME.''.DIR.'/player.swf",
+					playlist:['.$this->read('source').'],
+					width: "'.$this->read('width').'",
+					height: "'.$this->read('height').'",
+					logo: {
+						file : "'.$this->read('logo_file').'",
+						link : "'.$this->read('logo_link').'",
+						position : "'.$this->read('logo_position').'",
+					},
+					skin:{url:"//'.NAME.''.DIR.'/assets/skin/skin-haplugin.css",name:"haplugin"},
+					'.$this->__handle('videoads').'
+					'.$this->__handle('share').'
+				});
+				jwplayer("myElement").onCaptionsList(function() {
+					this.setCurrentCaptions(1);
+				});
+				</script>
+				<script type="text/javascript">
+					'.$this->__handle('download').'
+				</script>';
+		return $html;
 	 }
 	 public function result()
 	 {
-		 return $this->__handle();
+		 return $this->__html();
 	 }
- }
+}
 ?>
