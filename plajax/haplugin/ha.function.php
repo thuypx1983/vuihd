02094a71d76e2abb63c8147d2442079a3a487017
PHAM XUAN THUY <thuypx1983@gmail.com>
2017-02-28 20:34:42 +0700

    player

--- plajax/haplugin/ha.function.php
+++ plajax/haplugin/ha.function.php
@@ -785,7 +785,7 @@ class HAPlugin
 		//PLAYER START
 		$player = '<style type="text/css">#player-content{position:relative;padding-bottom:56.25%;height:0;overflow:hidden;max-width:100%}#player-content iframe,#player-content object,#player-content embed{position:absolute;top:0;left:0;width:100%;height:100%}.spinner{width:40px;height:40px;position:relative;margin:100px auto}.double-bounce1,.double-bounce2{width:100%;height:100%;border-radius:50%;background-color:#333;opacity:.6;position:absolute;top:0;left:0;-webkit-animation:sk-bounce 2s infinite ease-in-out;animation:sk-bounce 2s infinite ease-in-out}.double-bounce2{-webkit-animation-delay:-1s;animation-delay:-1s}@-webkit-keyframes sk-bounce{0%,100%{-webkit-transform:scale(0.0)}50%{-webkit-transform:scale(1.0)}}@keyframes sk-bounce{0%,100%{transform:scale(0.0);-webkit-transform:scale(0.0)}50%{transform:scale(1.0);-webkit-transform:scale(1.0)}}</style><div id="player-content"><div class="spinner"><div class="double-bounce1"></div><div class="double-bounce2"></div></div></div>
 		<script type="text/javascript">var sitename = "vuihd.com/plajax/haplugin/";</script>
-		<script type="text/javascript" src="//vuihd.localhost/plajax/haplugin/embed.js"></script>
+		<script type="text/javascript" src="//vuihd.com/plajax/haplugin/embed.js"></script>
 		<script type="text/javascript">'.$data_play.'</script>';
 		//PLAYER END
 		return $player;
