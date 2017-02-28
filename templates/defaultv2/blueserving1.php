a253649c978029837b15ba18ff626f64cdd90e3e
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2017-02-28 18:04:18 +0700

    player add

---
+++ templates/defaultv2/blueserving1.php
@@ -0,0 +1,8 @@
+<?php
+require_once('plajax/haplugin/license.php');
+require_once('plajax/haplugin/ha.function.php');
+$ha = new HAPlugin;
+?>
+<div>
+    <div id="player-area"><?php echo $ha->handle('https://www.youtube.com/watch?v=BNdRb9UC_qs',NULL,NULL,'video','vietnam');?> </div>
+</div>
